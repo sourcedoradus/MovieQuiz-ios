d03a25b947e254f592292ea319da08a31ff18fe3
sourcedoradus <valentine.medvedev@gmail.com>
2024-07-16 17:26:42 -0300

    refactor: extract Models from MovieQuizViewController

---
+++ MovieQuiz/Models/QuizResultsViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  QuizResultsViewModel.swift
+//  MovieQuiz
+//
+//  Created by Valentin Medvedev on 16.07.2024.
+//
+
+import Foundation
+
+struct QuizResultsViewModel {
+    let title: String
+    let text: String
+    let buttonText: String
+}
---
+++ MovieQuiz/Models/QuizStepViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  QuizStepViewModel.swift
+//  MovieQuiz
+//
+//  Created by Valentin Medvedev on 16.07.2024.
+//
+
+import Foundation
+import UIKit
+
+struct QuizStepViewModel {
+    let image: UIImage
+    let question: String
+    let questionNumber: String
+}
