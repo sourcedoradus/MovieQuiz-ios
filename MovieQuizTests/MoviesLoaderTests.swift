3386f7f822c17d56383d16422ae5bb459d52f959
sourcedoradus <valentine.medvedev@gmail.com>
2024-08-20 20:44:04 -0300

    refactor: remove debugging code from tests

--- MovieQuizTests/MoviesLoaderTests.swift
+++ MovieQuizTests/MoviesLoaderTests.swift
@@ -6,16 +6,16 @@
 //
 
 import Foundation
-import XCTest // не забывайте импортировать фреймворк для тестирования
-@testable import MovieQuiz // импортируем приложение для тестирования
+import XCTest
+@testable import MovieQuiz
 
 struct StubNetworkClient: NetworkRouting {
     
-    enum TestError: Error { // тестовая ошибка
+    enum TestError: Error {
     case test
     }
     
-    let emulateError: Bool // этот параметр нужен, чтобы заглушка эмулировала либо ошибку сети, либо успешный ответ
+    let emulateError: Bool
     
     func fetch(url: URL, handler: @escaping (Result<Data, Error>) -> Void) {
         if emulateError {
@@ -62,18 +62,16 @@ struct StubNetworkClient: NetworkRouting {
 
 class MoviesLoaderTests: XCTestCase {
     func testSuccessLoading() throws {
-        // Given
-        let stubNetworkClient = StubNetworkClient(emulateError: false) // говорим, что не хотим эмулировать ошибку
+        
+        let stubNetworkClient = StubNetworkClient(emulateError: false)
         let loader = MoviesLoader(networkClient: stubNetworkClient)
         
-        // When
         let expectation = expectation(description: "Loading expectation")
         
         loader.loadMovies { result in
-            // Then
+            
             switch result {
             case .success(let movies):
-                // давайте проверим, что пришло, например, два фильма — ведь в тестовых данных их всего два
                 XCTAssertEqual(movies.items.count, 2)
                 expectation.fulfill()
             case .failure(_):
@@ -85,15 +83,14 @@ class MoviesLoaderTests: XCTestCase {
     }
     
     func testFailureLoading() throws {
-        // Given
-        let stubNetworkClient = StubNetworkClient(emulateError: true) // говорим, что хотим эмулировать ошибку
+        
+        let stubNetworkClient = StubNetworkClient(emulateError: true)
         let loader = MoviesLoader(networkClient: stubNetworkClient)
         
-        // When
         let expectation = expectation(description: "Loading expectation")
         
         loader.loadMovies { result in
-            // Then
+            
             switch result {
             case .failure(let error):
                 XCTAssertNotNil(error)
