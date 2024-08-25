9a01c7bd5d1686215e3b0485d230411f7a76e4c8
sourcedoradus <valentine.medvedev@gmail.com>
2024-08-25 14:50:46 -0300

    feat: add random rating threshold for quiz questions

--- MovieQuiz/Services/QuestionFactory.swift
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -34,9 +34,10 @@ final class QuestionFactory: QuestionFactoryProtocol {
             }
             
             let rating = Float(movie.rating) ?? 0
+            let randomThreshold = Int.random(in: 7...9)
             
-            let text = "Рейтинг этого фильма больше чем 7?"
-            let correctAnswer = rating > 7
+            let text = "Рейтинг этого фильма больше чем \(randomThreshold)?"
+            let correctAnswer = rating > Float(randomThreshold)
             
             let question = QuizQuestion(image: imageData,
                                         text: text,
